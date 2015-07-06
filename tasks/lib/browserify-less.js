b1e4aa614c412598b38536e5b35632b4a39162a5
Thomas Wilburn <news@thomaswilburn.net>
2015-07-06 15:51:47 -0700

    Roll our own NPM importer for LESS
    The plugin assumes we're a top-level module, but of course this is never
    a top-level module, so we need to specifically resolve from the
    transform, not from the current directory.

--- tasks/lib/browserify-less.js
+++ tasks/lib/browserify-less.js
@@ -1,7 +1,44 @@
 var less = require("less");
 var path = require("path");
 var through = require("through2");
-var NpmImport = require("less-plugin-npm-import");
+var resolve = require("resolve").sync;
+
+// we need to plug into the LESS import statement for node modules
+// Sadly, the less-plugin-npm-import module doesn't handle this well
+// It assumes that we have all our dependencies at the top level
+var npmImporter = {
+  install: function(less, pluginManager) {
+
+    var FileManager = function() {};
+    FileManager.prototype = new less.FileManager();
+    FileManager.prototype.supports = function(file, dir) {
+      return file.indexOf("npm://") == 0;
+    };
+    FileManager.prototype.supportsSync = FileManager.prototype.supports;
+    FileManager.prototype.resolve = function(file) {
+      file = file.replace("npm://", "");
+      var resolved = resolve(file, {
+        extensions: [".less", ".css"],
+        packageFilter: function(package) { 
+          if (package.style) package.main = package.style;
+          return info;
+        }
+      });
+      return resolved;
+    };
+    FileManager.prototype.loadFile = function(url, dir, options, env) {
+      var filename = this.resolve(url);
+      return less.FileManager.prototype.loadFile.call(this, filename, "", options, env);
+    };
+    FileManager.prototype.loadFileSync = function(url, dir, options, env) {
+      var filename = this.resolve(url);
+      return less.FileManager.prototype.loadFileSync.call(this, filename, "", options, env);
+    };
+
+    pluginManager.addFileManager(new FileManager());
+  },
+  minVersion: [2, 1, 1]
+};
 
 var _ = function(tmp) {
   return tmp.toString().replace(/^.*\/\*\n*\s*|\*\/\}$/gm, "");
@@ -25,7 +62,7 @@ module.exports = function(file) {
       var self = this;
       var options = {
         paths: [ path.dirname(file) ],
-        plugins: [ new NpmImport() ]
+        plugins: [ npmImporter ]
       };
 
       less.render(buffer, options, function(err, result) {
